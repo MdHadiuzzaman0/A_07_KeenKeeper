6a73a6e7ff3de1c3fe007adf1d8324f71a3a69dc
Mdhadiuzzaman0 <mdhadiuzzaman483@gmail.com>
2026-04-15 23:41:03 +0600

    Stats section complete

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense, use, useContext } from 'react';
 // import { DataCenter } from './MotherProvider';
 import HeroSection from './HeroSection';
-// import StatsSectionInHome from './StatsSectionInHome';
+import StatsSectionInHome from './StatsSectionInHome';
 // import Friends from './Friends';
 import { HiH1 } from 'react-icons/hi2';
 import { useLoaderData } from 'react-router';
@@ -15,12 +15,12 @@ const Home = () => {
         <div>
             <div className='space-y-5 mt-9 px-30'>
                 <HeroSection />
-                {/* <StatsSectionInHome friendsData={friendsData} />
+                <StatsSectionInHome friendsData={friendsData} />
                 <hr className='opacity-10 my-9'/>
                 <h3 className='text-2xl font-semibold'>Your Friends</h3>
-                <Suspense fallback={<h1>Loading...</h1>}>
+                {/* <Suspense fallback={<h1>Loading...</h1>}>
                    <Friends />
-                </Suspense> */}
+                </Suspense>  */}
               
                 
             </div>
---
+++ src/StatsSectionInHome.jsx
@@ -0,0 +1,41 @@
+import React, { use, useContext } from 'react';
+// import { DataCenter } from './MotherProvider';
+
+const StatsSectionInHome = ({friendsData}) => {
+    
+    // const friends = friendsData;
+    // const onTrackLength = friends.filter(friend => friend.status === "on-track").length;
+    // const overdueLength = friends.filter(friend => friend.status === "overdue").length;
+    // console.log(onTrackLength);
+    
+    // const {clickedData} = useContext(DataCenter);
+    // const statsClass = `bg-white border border-gray-200 rounded-xl p-6 text-center shadow-sm`;
+
+    return (
+        <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
+                <div className={statsClass}>
+                    <h2 className="text-3xl font-bold text-emerald-900">{friends.length}</h2>
+                    <p className="text-gray-500 mt-2 text-lg">Friends</p>
+                </div>
+
+                <div className={statsClass}>
+                    <h2 className="text-3xl font-bold text-emerald-900"> {onTrackLength} </h2>
+                    <p className="text-gray-500 mt-2 text-lg">On-track</p>
+                </div>
+
+                <div className={statsClass}>
+                    <h2 className="text-3xl font-bold text-emerald-900"> {overdueLength} </h2>
+                    <p className="text-gray-500 mt-2 text-lg">Need Attention</p>
+                </div>
+
+                <div className={statsClass}>
+                    <h2 className="text-3xl font-bold text-emerald-900">{clickedData.length}</h2>
+                    <p className="text-gray-500 mt-2 text-lg">Interactions this month</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StatsSectionInHome;
